cdcba44d8d265f4cf1b56f8853a1fb13af33a404
Gabriel S. Olavo <143572039+gabrielolavo3@users.noreply.github.com>
2023-10-02 02:27:36 -0300

    Atualizar o 7. Escolhendo produto a partir do preço.c

--- Visual Studio Code - Linguagem C/02. Exercises at Home/7. Escolhendo produto a partir do preço.c
+++ Visual Studio Code - Linguagem C/02. Exercises at Home/7. Escolhendo produto a partir do preço.c
@@ -1,5 +1,5 @@
 /* 
-Descrição   : Analisar o preço de 3 produtos e optar em comprar aquele com menor preço! (função)
+DescriÃ§Ã£o   : Analisar o preÃ§o de 3 produtos e optar em comprar aquele com menor preÃ§o! (funÃ§Ã£o)
 Autor(a)    : Gabriel S. Olavo
 Data atual  : 02/10/2023
 */
@@ -11,37 +11,37 @@ Data atual  : 02/10/2023
 int main () {
     setlocale (LC_ALL, "portuguese");
 
-// Declaração de variáveis
+// DeclaraÃ§Ã£o de variÃ¡veis
 
     float primeiroProduto, segundoProduto, terceiroProduto;
 
 // Solicitando dados
 
-    printf ("Informe o valor (em R$) do 1º produto: ");
+    printf ("Informe o valor (em R$) do 1Âº produto: ");
     scanf ("%f", &primeiroProduto);
-    printf ("Informe o valor (em R$) do 2º produto: ");
+    printf ("Informe o valor (em R$) do 2Âº produto: ");
     scanf ("%f", &segundoProduto);
-    printf ("Informe o valor (em R$) do 3º produto: ");
+    printf ("Informe o valor (em R$) do 3Âº produto: ");
     scanf ("%f", &terceiroProduto);
 
-system ("cls || claer");
+system ("cls || clear");
 
 // Tela de resultado
     printf ("ESCOLHA DO PRODUTO MAIS BARATO! \n\n");
-    printf ("Valor do 1º produto: %.2f R$ \n", primeiroProduto);
-    printf ("Valor do 2º produto: %.2f R$ \n", segundoProduto);
-    printf ("Valor do 3º produto: %.2f R$ \n\n", terceiroProduto);
+    printf ("Valor do 1Âº produto: %.2f R$ \n", primeiroProduto);
+    printf ("Valor do 2Âº produto: %.2f R$ \n", segundoProduto);
+    printf ("Valor do 3Âº produto: %.2f R$ \n\n", terceiroProduto);
 
 // Escolhendo qual produto comprar
 
 if (primeiroProduto < segundoProduto && primeiroProduto < terceiroProduto) {
-    printf ("Escolha final: O 1º produto deve ser adquirido");
+    printf ("Escolha final: O 1Âº produto deve ser adquirido");
 
 } else { if (segundoProduto < primeiroProduto && segundoProduto < terceiroProduto) {
-    printf ("Escolha final: O 2º produto deve ser adquirido"); } 
+    printf ("Escolha final: O 2Âº produto deve ser adquirido"); } 
     
 else { (terceiroProduto < segundoProduto && terceiroProduto < primeiroProduto); 
-    printf ("Escolha final: O 3º produto deve ser adquirido"); }
+    printf ("Escolha final: O 3Âº produto deve ser adquirido"); }
 
 }
     return 0;
