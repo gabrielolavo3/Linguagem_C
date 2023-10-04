092db571b3b4c4f6677ccbfbcb2c9e92fcfea154
Gabriel S. Olavo <143572039+gabrielolavo3@users.noreply.github.com>
2023-10-04 00:01:04 -0300

    Update and rename 3. Consumo de combustível.c to 03. Consumo de combustível.c

--- Visual Studio Code - Linguagem C/02. Exercises at Home/03. Consumo de combustível.c
+++ Visual Studio Code - Linguagem C/02. Exercises at Home/03. Consumo de combustível.c
@@ -5,24 +5,24 @@
 int main () {
     setlocale (LC_ALL, "portuguese");
     
-// DeclaraÁ„o de vari·veis
+// Declara√ß√£o de vari√°veis
     float distancia, combustivel, media;
 
 // Solicitando dados
-    printf ("Informe a dist‚ncia percorrida (em KM): ");
+    printf ("Informe a dist√¢ncia percorrida (em KM): ");
     scanf ("%f", &distancia);
-    printf ("Informe a quantidade de combustÌvel utilizado (em L): ");
+    printf ("Informe a quantidade de combust√≠vel utilizado (em L): ");
     scanf ("%f", &combustivel);
 
 system ("cls || clear");
 
-// Calculando o consumo mÈdio de combustÌvel
+// Calculando o consumo m√©dio de combust√≠vel
     media = distancia / combustivel;
     
 // Tela de resultado
     printf ("KMs percorridos: %.2f Kms \n", distancia);
-    printf ("Litros de combustÌvel: %.2f L \n", combustivel);
-    printf ("Consumo mÈdio de combustÌvel: %.2f L \n", media);
+    printf ("Litros de combust√≠vel: %.2f L \n", combustivel);
+    printf ("Consumo m√©dio de combust√≠vel: %.2f L \n", media);
 
     return 0;
 }
