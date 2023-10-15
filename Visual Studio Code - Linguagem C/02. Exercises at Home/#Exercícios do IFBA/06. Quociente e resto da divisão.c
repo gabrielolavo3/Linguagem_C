756afdfe08fa5e01d2ff6ebfdf60b336ccda5e0c
Gabriel S. Olavo <143572039+gabrielolavo3@users.noreply.github.com>
2023-10-15 02:22:32 -0300

    Add files via upload

---
+++ Visual Studio Code - Linguagem C/02. Exercises at Home/#Exercícios do IFBA/06. Quociente e resto da divisão.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main () {
+    setlocale (LC_ALL, "portuguese");
+
+// Declaração de variáveis
+
+    int primeiroNumero, segundoNumero;
+    float quociente, restodaDivisao;
+
+// Solicitando dados
+
+    printf ("MINI CALCULADORA DE DIVISÃO!\n\n");
+    printf ("Insira o 1º número: ");
+    scanf ("%d", &primeiroNumero);
+    printf ("Insira o 2º número: ");
+    scanf ("%d", &segundoNumero);
+
+// Calculando o quociente e o resto da divisão
+
+quociente = primeiroNumero / segundoNumero;
+restodaDivisao = primeiroNumero % segundoNumero;
+system ("cls || clear");
+
+// Tela de resultado
+
+    printf ("MINI CALCULADORA DE DIVISÃO!\n\n");
+    printf ("1º número: %.0d\n", primeiroNumero);
+    printf ("2º número: %.0d\n", segundoNumero);
+    printf ("Quociente da divisão: %.0f\n", quociente);
+    printf ("Resto da divisão: %.0f\n", restodaDivisao);
+
+    return 0;
+}
