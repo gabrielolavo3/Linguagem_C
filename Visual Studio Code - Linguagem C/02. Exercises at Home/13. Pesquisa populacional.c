5eff7502e5a5bcce2268c16d11f4dfc964887a49
Gabriel S. Olavo <143572039+gabrielolavo3@users.noreply.github.com>
2023-10-18 16:17:05 -0300

    Update 13. Pesquisa populacional.c

--- Visual Studio Code - Linguagem C/02. Exercises at Home/13. Pesquisa populacional.c
+++ Visual Studio Code - Linguagem C/02. Exercises at Home/13. Pesquisa populacional.c
@@ -1,6 +1,6 @@
 /* 
-DescriÁ„o  : Com base em uma pesquisa populacioanl com X entrevistados, solicitar o sexo, idade e sal·roi de cada entrevistado.
-Com isso, imprimir a quantidade de pessoas, a maior e menor idade, e a quantidade de mulheres com sal·rio a partir de R$ 5.000,00! (funÁ„o)
+Descri√ß√£o  : Com base em uma pesquisa populacioanl com X entrevistados, solicitar o sexo, idade e sal√°roi de cada entrevistado.
+Com isso, imprimir a quantidade de pessoas, a maior e menor idade, e a quantidade de mulheres com sal√°rio a partir de R$ 5.000,00! (fun√ß√£o)
 Professor  : Carlos Anderson Santos de Jesus
 Autor(a)   : Gabriel S. Olavo
 Data atual : 16/10/2023 */
@@ -13,7 +13,7 @@ Data atual : 16/10/2023 */
 int main () {
     setlocale (LC_ALL, "portuguese");
 
-// DeclaraÁ„o de vari·veis
+// Declara√ß√£o de vari√°veis
 
     int a = 0, opcao, entrevistado, idade, maiorIdade = 0, menorIdade = 120, quantidadeDeMulheres = 0;
     char sexo; 
@@ -22,11 +22,11 @@ int main () {
 // Solicitando dados
 
 do {
-    printf ("PESQUISA POPULACIONAL - INSTITUTO DE ESTATÕSTICA NACIONAL (IEN)\n\n");
-    printf ("CÛdigo | \tDescriÁ„o\n");
-    printf ("   1   | Adicionar informaÁıes\n");
+    printf ("PESQUISA POPULACIONAL - INSTITUTO DE ESTAT√çSTICA NACIONAL (IEN)\n\n");
+    printf ("C√≥digo | \tDescri√ß√£o\n");
+    printf ("   1   | Adicionar informa√ß√µes\n");
     printf ("   2   | Sair e exibir resultados\n\n");
-    printf ("Selecione a opÁ„o desejada: ");
+    printf ("Selecione a op√ß√£o desejada: ");
     scanf ("%d", &opcao);
 
 switch (opcao) {
@@ -36,14 +36,14 @@ case 1:
 
 for (a = 1; a <= entrevistado; a++) {
 
-// Definindo o tÌtulo conforme a quantidade de entrevistados
+// Definindo o t√≠tulo conforme a quantidade de entrevistados
 
 if (entrevistado > 1) {
-    printf ("\nINSIRA OS DADOS DO %d∫ ENTREVISTADO \n", a); 
+    printf ("\nINSIRA OS DADOS DO %d¬∫ ENTREVISTADO \n", a); 
 } else {
     printf ("\nINSIRA OS DADOS DO ENTREVISTADO \n", a); }
 
-// Limpando o cache, lendo o sexo e validando a informaÁ„o
+// Limpando o cache, lendo o sexo e validando a informa√ß√£o
 
 do {
     fflush (stdin);
@@ -55,26 +55,28 @@ do {
     sexo = toupper (sexo);
 
     if (sexo != 'M' && sexo != 'F') {
-    printf ("O sexo informado È INV¡LIDO! Digite 'F' ou 'f para feminino e 'M' ou 'm' para masculino \n"); }
+    printf ("O sexo informado √© INV√ÅLIDO! Digite 'F' ou 'f para feminino e 'M' ou 'm' para masculino \n"); }
 } while (sexo != 'M' && sexo != 'F');
 
+// Lendo a idade e validando a informa√ß√£o
+    
 do {
     printf ("Informe a idade do(a) entrevistado(a): ");
     scanf ("%d", &idade);
 
     if (idade < maiorIdade || idade > menorIdade) {
-    printf ("\nA idade dos entrevistados È LIMITADA entre %d e %d anos! Digite a idade novamente \n", maiorIdade, menorIdade); }  
+    printf ("\nA idade dos entrevistados √© LIMITADA entre %d e %d anos! Digite a idade novamente \n", maiorIdade, menorIdade); }  
 } while (idade < maiorIdade || idade > menorIdade);
 
-    printf ("Informe o sal·rio do(a) entrevistado(a): ");
+    printf ("Informe o sal√°rio do(a) entrevistado(a): ");
     scanf ("%f", &salario);
 
-// Acumulando os valores de sal·rios elimpando o terminal
+// Acumulando os valores de sal√°rios elimpando o terminal
     
     system ("cls || clear");
     somadeSalario += salario; 
 
-// Analisando a maior, menor idade e a quantidade de mulheres com sal·rio a partir de R$ 5.000,00
+// Analisando a maior, menor idade e a quantidade de mulheres com sal√°rio a partir de R$ 5.000,00
 
     maiorIdade = idade > maiorIdade ? idade : maiorIdade;
     menorIdade = idade < menorIdade ? idade : menorIdade;
@@ -89,17 +91,17 @@ case 2:
 system ("cls || clear");
 mediaSalarial = (float) somadeSalario / (float) entrevistado;
 
-    printf ("INSTITUTO DE ESTATÕSTICA NACIONAL (IEN)\nRESULTADOS PESQUISA POPULACIONAL \n\n");
+    printf ("INSTITUTO DE ESTAT√çSTICA NACIONAL (IEN)\nRESULTADOS PESQUISA POPULACIONAL \n\n");
     printf ("Quantidade de entrevistados: %d \n", entrevistado);
     printf ("Maior idade entre os entrevistados: %d \n", maiorIdade);
     printf ("Menor idade entre os entrevistados: %d \n", menorIdade);
-    printf ("Mulheres com sal·rio acima de R$ 5000,00: %d \n", quantidadeDeMulheres);
-    printf ("MÈdia salarial: %.2f \n", mediaSalarial);
+    printf ("Mulheres com sal√°rio acima de R$ 5000,00: %d \n", quantidadeDeMulheres);
+    printf ("M√©dia salarial: %.2f \n", mediaSalarial);
     break;
        
 default:
 system ("cls || clear");
-    printf ("OP«√O SELECIONDA … INV¡LIDA! Retorne ‡ tela de aÁıes. \n\n");
+    printf ("OP√á√ÉO SELECIONDA √â INV√ÅLIDA! Retorne √† tela de a√ß√µes. \n\n");
     break; }
     
 } while (opcao != 2);
