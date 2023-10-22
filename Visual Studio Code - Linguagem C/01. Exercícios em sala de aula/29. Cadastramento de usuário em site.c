f1846dca3b9718532d1e9b696afc2ea22ee2b61e
Gabriel S. Olavo <143572039+gabrielolavo3@users.noreply.github.com>
2023-10-22 05:01:42 -0300

    Atualizar o 29. Cadastramento de usuário em site.c

--- Visual Studio Code - Linguagem C/01. Exercícios em sala de aula/29. Cadastramento de usuário em site.c
+++ Visual Studio Code - Linguagem C/01. Exercícios em sala de aula/29. Cadastramento de usuário em site.c
@@ -1,5 +1,5 @@
 /* 
-DescriÁ„o  : Ler os dados de usu·rio e senha inseridos e verificar as informaÁıes! (funÁ„o)
+Descri√ß√£o  : Ler os dados de usu√°rio e senha inseridos e verificar as informa√ß√µes! (fun√ß√£o)
 Professor  : Carlos Anderson Santos de Jesus
 Autor(a)   : Gabriel S. Olavo
 Data atual : 19/10/2023 */
@@ -13,36 +13,36 @@ Data atual : 19/10/2023 */
 int main ( ) {
     setlocale (LC_ALL, "portuguese");
     
-// DeclaraÁ„o de vari·veis
+// Declara√ß√£o de vari√°veis
 
     char usuario [250], login [250], senha [250], password [250];
 
 // Solicitando dados
 
-    printf ("BEM VINDO AO SITE DO INSTITUTO DE ESTATISTICA NACIONAL (IEN) \n\n");
-    printf ("Vejo que vocÍ È novo por aqui. Por favor, crie um cadastro \n\n");
-    printf ("Crie um nome para o usu·rio: ");
+    printf ("BEM VINDO AO SITE DO INSTITUTO DE ESTA√çISTICA NACIONAL (IEN) \n\n");
+    printf ("Vejo que voc√™ √© novo por aqui. Por favor, crie um cadastro \n\n");
+    printf ("Crie um nome para o usu√°rio: ");
     gets (usuario);
-    printf ("Crie uma senha para o seu novo nome de usu·rio: ");
+    printf ("Crie uma senha para o seu novo nome de usu√°rio: ");
     gets (senha);
     printf ("\nCom o seu novo cadastro, realize a login em nosso site \n");
 
 system ("pause");
 system ("cls || clear");
 
-// ValidaÁ„o dos dados de cadastro e tela de resultado
+// Valida√ß√£o dos dados de cadastro e tela de resultado
 
 do {
-    printf ("BEM VINDO AO INSTITUTO DE ESTATISTICA NACIONAL (IEN) \n\n");
-    printf ("Informe o nome de usu·rio: ");
+    printf ("BEM VINDO AO INSTITUTO DE ESTAT√çSTICA NACIONAL (IEN) \n\n");
+    printf ("Informe o nome de usu√°rio: ");
     gets (login);
-    printf ("Informe a senha de usu·rio: ");
+    printf ("Informe a senha de usu√°rio: ");
     gets (password);
 
     if (strcmp (usuario, login) == 0 && strcmp (senha, password) == 0) {
         printf ("Login confirmado. SEJA BEM-VINDO! \n"); 
     } else {
-        printf ("Senha ou usu·rio INV¡LIDOS! Por favor, informe os dados novamente \n\n");
+        printf ("Senha ou usu√°rio INV√ÅLIDOS! Por favor, informe os dados novamente \n\n");
         sleep (1); }
     
 } while (strcmp (usuario, login) != 0 || strcmp (senha, password) != 0); 
