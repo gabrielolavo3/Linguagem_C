995abb552e8c800546b5c474677b0b7b24508e35
Gabriel S. Olavo <143572039+gabrielolavo3@users.noreply.github.com>
2023-11-09 16:27:32 -0300

    Update 34. Maior e menor com vetor.c

--- Visual Studio Code - Linguagem C/01. Exercícios em sala de aula/34. Maior e menor.c
+++ Visual Studio Code - Linguagem C/01. Exercícios em sala de aula/34. Maior e menor.c
@@ -5,17 +5,26 @@
 int main ( ) {
     setlocale (LC_ALL, "");
 
-// DeclaraÁ„o de vari·veis
+// Declara√ß√£o de vari√°veis
 
     int a, tamanho = 5;
-    int numero[tamanho], maior, menor;
+    int numero[tamanho], maior = 0, menor = 999;
 
 // Solicitando dados
 
     for (a = 0; a < 5; a++) {
-        printf ("Informe o %d∫ n˙mero: ", a+1);
+        printf ("Informe o %d¬∫ n√∫mero: ", a+1);
         scanf ("%d", &numero[a]);
-    }
+
+    maior = numero[a] > maior ? numero[a] : maior;
+    menor = numero[a] < menor ? numero[a] : menor; }
+
+/*
+    if (numero[a] > maior) {
+        maior = numero[a]; }
+    
+    if (numero[a] < menor) {
+        menor = numero[a]; }
 
     maior = numero[0] > numero [1] ? numero [0] : numero [1];
     maior = maior > numero [2] ? maior : numero [2];
@@ -25,13 +34,13 @@ int main ( ) {
     menor = numero[0] < numero [1] ? numero [0] : numero [1];
     menor = menor < numero [2] ? menor : numero [2];
     menor = menor < numero [3] ? menor : numero [3];
-    menor = menor < numero [4] ? menor : numero [4];
+    menor = menor < numero [4] ? menor : numero [4]; */
 
     system ("cls");
     for (a = 0; a < 5; a++) {
-    printf ("%d∫ n˙mero: %d\n", a+1, numero[a]); }
-    printf ("Maior n˙mero: %d\n", maior);
-    printf ("Menor n˙mero: %d\n", menor);
+    printf ("%d¬∫ n√∫mero: %d\n", a+1, numero[a]); }
+    printf ("Maior n√∫mero: %d\n", maior);
+    printf ("Menor n√∫mero: %d\n", menor);
 
     return 0;
 }
