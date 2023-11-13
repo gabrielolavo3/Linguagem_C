a43637a5516faa9f08ac7f33f28c75a821622a96
Gabriel S. Olavo <143572039+gabrielolavo3@users.noreply.github.com>
2023-11-12 22:00:45 -0300

    Atualizar o 15. Nome e idade com vetor.c

--- Visual Studio Code - Linguagem C/02. Exercises at Home/15. Nome e idade com vetor.c
+++ Visual Studio Code - Linguagem C/02. Exercises at Home/15. Nome e idade com vetor.c
@@ -1,5 +1,6 @@
 /* 
-DescriÁ„o  : Usar um vetor para ler a idade e nome de 5 pessoas. No final, imprimir os dados! (funÁ„o)
+Descri√ß√£o  : Usar um vetor para ler a idade e nome de 5 pessoas. No final, imprimir os dados! (fun√ß√£o)
+Professor  : Carlos Anderson Santos de Jesus
 Autor(a)   : Gabriel S. Olavo
 Data atual : 11/10/2023 */
 
@@ -11,7 +12,7 @@ Data atual : 11/10/2023 */
 int main ( ) {
     setlocale (LC_ALL, "portuguese");
 
-// DeclaraÁ„o de vari·veis
+// Declara√ß√£o de vari√°veis
 
     int a, b = 5;
     int idade [b];
@@ -19,11 +20,11 @@ int main ( ) {
 
 // Solicitando dados
 
-    printf ("STEEL MOUNTAIN!\nColeta de dados de funcion·rios\n\n");
+    printf ("STEEL MOUNTAIN!\nColeta de dados de funcion√°rios\n\n");
     for (a = 0; a < b; a++) {
-        printf ("Informe o nome do %d∫ colaborador: ", a+1);
+        printf ("Informe o nome do %d¬∫ colaborador: ", a+1);
         gets (nome[a]);
-        printf ("Informe a idade do %d∫ colaborador: ", a+1);
+        printf ("Informe a idade do %d¬∫ colaborador: ", a+1);
         scanf ("%d", &idade[a]); 
         fflush(stdin);
         printf ("\n"); }
@@ -36,7 +37,7 @@ int main ( ) {
     system ("cls || clear");
     printf ("STEEL MOUNTAIN!\nResumo dos dados inseridos\n\n");
     for (a = 0; a < b; a++) {
-        printf ("Nome e idade do %d™ colaborador: %s - %d anos\n", a+1, nome[a], idade[a]); }
+        printf ("Nome e idade do %d¬™ colaborador: %s - %d anos\n", a+1, nome[a], idade[a]); }
 
     return 0;
 }
