556ab90a9cafd9e339e3168d2c119fa9a2a521f4
Gabriel S. Olavo <143572039+gabrielolavo3@users.noreply.github.com>
2023-11-15 02:50:18 -0300

    Atualizar o 21. Sexo e média de idade.c

--- Visual Studio Code - Linguagem C/02. Exercises at Home/#Exercícios de Laço de Repetição/21. Sexo e média de idade.c
+++ Visual Studio Code - Linguagem C/02. Exercises at Home/#Exercícios de Laço de Repetição/21. Sexo e média de idade.c
@@ -1,6 +1,5 @@
 /* 
-DescriÁ„o  : Ler o sexo e idade de 7 pessoas e imprimir a mÈdia geral das idades, a mÈdia das idades
-de mulheres e dos homens! (funÁ„o)
+Descri√ß√£o  : Ler o sexo e idade de 7 pessoas e imprimir a m√©dia geral das idades, a m√©dia das idades de mulheres e dos homens! (fun√ß√£o)
 Autor(a)   : Gabriel S. Olavo
 Data atual : 15/11/2023 */
 
@@ -12,7 +11,7 @@ Data atual : 15/11/2023 */
 int main ( ) {
     setlocale(LC_ALL, "portuguese");
 
-// DeclaraÁ„o de vari·veis
+// Declara√ß√£o de vari√°veis
 
     int a = 0;
     int idade;
@@ -23,12 +22,12 @@ int main ( ) {
 
 // Solicitando dados
 
-    printf ("ORGANIZA«√O HIGH CUBE! COLETANDO DADOS E DIVULGANDO PESQUISAS\nInforme dos dados requeridos\n\n");
+    printf ("ORGANIZA√á√ÉO HIGH CUBE! COLETANDO DADOS E DIVULGANDO PESQUISAS\nInforme os dados requeridos\n\n");
 
     for (a = 1; a <= 7; a++) {
         do {
             fflush(stdin);
-            printf("Insira o sexo ('M' ou 'F') do %d∫ entrevistado: ", a);
+            printf("Insira o sexo ('M' ou 'F') do %d¬∫ entrevistado: ", a);
             scanf("%c", &sexo);
             sexo = toupper(sexo);
 
@@ -37,16 +36,16 @@ int main ( ) {
             printf("\n");
 
             if (idade <= 0 || (sexo != 'F' && sexo != 'M')) {
-            printf ("H· dados inv·lidos! Informe o sexo ou idade CORRETAMENTE...\n\n"); }
+            printf ("H√° dados inv√°lidos! Informe o sexo ou idade CORRETAMENTE...\n\n"); }
 
         } while (idade <= 0 || (sexo != 'F' && sexo != 'M'));
 
-// Acumulando e calculando a mÈdia de todas as idades
+// Acumulando e calculando a m√©dia de todas as idades
  
         IdadeGeral += idade;
         mediaGeral = (float)IdadeGeral / (float) a;
 
-// Acumulando a quantidade de mulheres e homens, suas idades e calculando a mÈdia de idades
+// Acumulando a quantidade de mulheres e homens, suas idades e calculando a m√©dia de idades
 
         if (sexo == 'F') {
             somaIdade_Mulheres += idade;
@@ -63,10 +62,10 @@ int main ( ) {
 
     system ("cls");
 
-    printf ("ORGANIZA«√O HIGH CUBE! COLETANDO DADOS E DIVULGANDO PESQUISAS\nResumo dos dados estatÌsticos\n\n");
-    printf("MÈdia de idade das mulheres: %.0f\n", mediaMulheres);
-    printf("MÈdia de idade dos homens: %.0f\n", mediaHomens);
-    printf("MÈdia geral de idades: %.0f", mediaGeral);
+    printf ("ORGANIZA√á√ÉO HIGH CUBE! COLETANDO DADOS E DIVULGANDO PESQUISAS\nResumo dos dados estat√≠sticos\n\n");
+    printf("M√©dia de idade das mulheres: %.0f\n", mediaMulheres);
+    printf("M√©dia de idade dos homens: %.0f\n", mediaHomens);
+    printf("M√©dia geral de idades: %.0f", mediaGeral);
 
     return 0;
 }
