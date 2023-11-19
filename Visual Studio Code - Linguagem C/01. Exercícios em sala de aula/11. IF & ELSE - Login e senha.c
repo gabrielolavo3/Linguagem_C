928b7e699133241245a307a4f5e823d751761a3c
Gabriel S. Olavo <143572039+gabrielolavo3@users.noreply.github.com>
2023-11-19 15:51:41 -0300

    Update and rename 11. Login e senha.c to 11. IF & ELSE - Login e senha.c

--- Visual Studio Code - Linguagem C/01. Exercícios em sala de aula/11. IF & ELSE - Login e senha.c
+++ Visual Studio Code - Linguagem C/01. Exercícios em sala de aula/11. IF & ELSE - Login e senha.c
@@ -1,5 +1,5 @@
 /* 
-DescriÁ„o  : Realizar um cadastro online e verificar o acesso correto a conta atravÈs dos dados exatos! (funÁ„o)
+Descri√ß√£o  : Realizar um cadastro online e verificar o acesso correto a conta atrav√©s dos dados exatos! (fun√ß√£o)
 Autor(a)   : Gabriel S. Olavo
 Data atual : 02/10/2023 */
 
@@ -11,7 +11,7 @@ Data atual : 02/10/2023 */
 int main () {
     setlocale (LC_ALL, "portuguese");
 
-// DeclaraÁ„o de vari·veis
+// Declara√ß√£o de vari√°veis
 
     char usuario [200];
     char chave [200];
@@ -21,11 +21,11 @@ int main () {
 // Solicitando dados
 
     printf ("BEM-VINDO AO SITE DO BC! Realize o seu cadastro. \n\n");
-    printf ("Crie um nome para o usu·rio: ");
+    printf ("Crie um nome para o usu√°rio: ");
     gets (usuario);
-    printf ("Crie uma senha para o usu·rio %s: ", usuario);
+    printf ("Crie uma senha para o usu√°rio %s: ", usuario);
     gets (chave);
-    printf ("\nCom o seu novo cadastro, realize o seu 1∫ login em nosso site.\n");
+    printf ("\nCom o seu novo cadastro, realize o seu 1¬∫ login em nosso site.\n");
 
 // Pausando e limpando o terminal
 
@@ -35,9 +35,9 @@ system ("cls || clear");
 // Tela de resultado
 
     printf ("BEM-VINDO AO SITE DO BC! \n\n");
-    printf ("Nome de usu·rio: ");
+    printf ("Nome de usu√°rio: ");
     gets (login);
-    printf ("Senha de usu·rio: ");
+    printf ("Senha de usu√°rio: ");
     gets (senha);
 
 // Validando o cadastro e o login com StringCompare
@@ -45,10 +45,10 @@ system ("cls || clear");
     if (strcmp (login, usuario) == 0 && strcmp (senha, chave) == 0) {
     printf ("\nBEM-VINDO AO SITE DO BC!");
     } else {
-    printf ("\nLOGIN OU SENHA INV¡LIDO! Insira seus dados novamente\n\n");
-    printf ("Nome de usu·rio: ");
+    printf ("\nLOGIN OU SENHA INV√ÅLIDO! Insira seus dados novamente\n\n");
+    printf ("Nome de usu√°rio: ");
     gets (login);
-    printf ("Senha de usu·rio: ");
+    printf ("Senha de usu√°rio: ");
     gets (senha); 
 
     if (strcmp (login, usuario) == 0 && strcmp (senha, chave) == 0) {
