7abb610c9d3c619d03f9c8073600cffea4c8a863
Gabriel S. Olavo <143572039+gabrielolavo3@users.noreply.github.com>
2023-11-21 16:50:10 -0300

    Create 45. Bandas.c

---
+++ Visual Studio Code - Linguagem C/01. Exercícios em sala de aula/45. Bandas.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main ( ) {
+    setlocale (LC_ALL, "");
+
+// Declaração de variáveis
+
+    int a,b;
+    char nome_Banda [3][250]; 
+    char integrante[3][5][250];
+
+// Solicitando dados
+
+    for (a = 0; a < 3; a++) {
+        printf ("Informe o nome da %dª banda: ", a+1);
+        gets (nome_Banda[a]);
+
+        for (b = 0; b < 5; b++) {
+            printf ("Digite o nome do %dº integrante: ", b+1);
+            gets (integrante[a][b]); }
+
+        printf ("\n");
+        fflush(stdin);
+    }
+
+// Tela de resultado
+
+    for (a = 0; a < 3; a++) {
+        printf ("Nome da banda: %s\n", nome_Banda[a]);
+
+        for (b = 0; b < 5; b++) {
+            printf ("Nome do %dº integrante: %s\n", b+1,integrante[a][b]); }
+
+        printf ("\n");
+    }
+
+
+    return 0;
+}
