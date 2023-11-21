01979c0c36faab74b01b27241ca6bbb7706c9458
Gabriel S. Olavo <143572039+gabrielolavo3@users.noreply.github.com>
2023-11-21 16:20:29 -0300

    Update 44. Aprovação de aluno.c

--- Visual Studio Code - Linguagem C/01. Exercícios em sala de aula/44. Aprovação de aluno.c
+++ Visual Studio Code - Linguagem C/01. Exercícios em sala de aula/44. Aprovação de aluno.c
@@ -4,7 +4,7 @@
 #include <locale.h>
 
 int main ( ) {
-    setlocale (LC_ALL, "");
+    setlocale (LC_ALL, "portuguese");
 
 // Declaração de variáveis
 
@@ -32,33 +32,34 @@ int main ( ) {
         somaNota = 0;
         setbuf (stdin, 0);
         fflush (stdin);
-    }
 
 // Situação estudantil
+
+        if (media[k] >= 7) {
+            strcpy (resultado[k], "Aprovado!"); 
+            
+        } else if (media[k] >= 5 && media[k] < 7) {
+            strcpy (resultado[k], "Recuperação!"); 
+
+        } else {
+            strcpy (resultado[k], "Reprovado!"); }
+    }
+
 
 // Tela de resultado
 
 system ("cls || clear");
 
     for (k = 0; k < 3; k++) {
-        printf ("Nome da disciplina: %s", nome_da_Disciplina[k]);
+        printf ("Nome da disciplina: %s\n", nome_da_Disciplina[k]);
 
         for (x = 0; x < 2; x++) {
             printf ("%d nota: %.2f\n",x+1,nota[k][x]); }
 
             printf ("Média: %.2f\n", media[k]);
-            
-
-            if (media[k] >= 7) {
-                printf ("Resultado final: Aprovado!"); 
-            
-            } else if (media[k] >= 5 && media[k] < 7) {
-                printf ("Resultado final: Recuperação!"); 
-
-            } else {
-                printf ("Resultado final: Recuperação!"); }
+            printf ("Resultado final: %s\n", resultado[k]);
             printf ("\n"); 
-            }
+        }
     
         return 0;
     }
