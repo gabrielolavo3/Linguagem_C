d3eccd68c947d0224484d4a1e5a14553b6226b88
Gabriel S. Olavo <143572039+gabrielolavo3@users.noreply.github.com>
2023-11-28 02:34:53 -0300

    Atualizar o Lista II de Aprendizagem

---
+++ Visual Studio Code - Linguagem C/02. Exercises at Home/#Exercícios de Vetores (Arrays)/Lista II de Aprendizagem/04. Exibindo posição dentro do vetor.c
@@ -0,0 +1,55 @@
+/* 
+Descrição  : Ler um vetor de 20 posições com números e ler uma variável X qualquer. Caso o valor de X seja igual à algum valor do vetor,
+imprimir a posição do número dentro do vetor! (função)
+Autor(a)   : Gabriel S. Olavo
+Data atual : 28/11/2023 */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+// Declaração de constantes
+
+#define b 10
+
+int main (void) {
+  setlocale (LC_ALL, "portuguese");
+
+// Declaração de variáveis
+
+  int a;
+  int contador = 0;
+  float x;
+  float vetor[b];
+
+// Solicitando dados
+  
+  printf ("NUMERIC SET! Contador de números em variáveis\n\n");
+  for (a = 0; a < b; a++) {
+    printf ("Informe o %d° valor: ", a+1);
+    scanf ("%f", &vetor[a]);
+  }
+
+  printf ("\n");
+
+  printf ("Informe o valor da posição X: ");
+  scanf("%f", &x);
+
+// Tela de resultado 
+
+system ("cls || clear");
+
+  printf ("NUMERIC SET!\n\n")
+  for (a = 0; a < b; a++) {
+    if (x == vetor[a]) {
+      printf ("Elemento localizado!\nElemento pertencente à posição %d do conjunto", a);
+    
+    } else {
+      contador++; }
+  }
+
+  if (contador == b) {
+    printf ("O elemento informado não foi localizado no conjunto de números"); }
+  
+  return 0;
+}
