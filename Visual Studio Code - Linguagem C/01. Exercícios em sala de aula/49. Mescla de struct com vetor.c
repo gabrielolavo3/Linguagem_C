bb4f4b14e6ab710d717cbe010c2f77b4819eba2a
Gabriel S. Olavo <143572039+gabrielolavo3@users.noreply.github.com>
2023-11-30 17:03:38 -0300

    Update 49. Mescla de struct com vetor.c

--- Visual Studio Code - Linguagem C/01. Exercícios em sala de aula/49. Mescla de struct com vetor.c
+++ Visual Studio Code - Linguagem C/01. Exercícios em sala de aula/49. Mescla de struct com vetor.c
@@ -4,36 +4,46 @@
 
 // Declaração de struct
 
-struct dados_pessoais {
-    
-    int idade;
-    char nome[250], endereco[250];
+struct livraria {
+    float preco;
+    char nome[250], autor[250], categoria[250];
 };
 
 int main ( ) {
-    setlocale (LC_ALL, "portuguese");
+    setlocale (LC_ALL, "");
 
-// Declaração de variável de acesso
+// Declaração de variáveis
 
-    struct dados_pessoais Dados;
+    int a;
+    struct livraria livros[3];
 
 // Solicitando dados
 
-    printf ("Informe o seu nome: ");
-    gets (Dados.nome);
-    printf ("Informe a sua idade: ");
-    scanf ("%d", &Dados.idade);
-    fflush (stdin);
-    printf ("Informe o seu endereço: ");
-    gets (Dados.endereco);
+    for (a = 0; a < 3; a++) {
+        printf ("Informe o nome do %dº livro: ", a+1);
+        gets (livros[a].nome);
+        printf ("Informe o autor: ");
+        gets (livros[a].autor);
+        printf ("Informe a categoria: ");
+        gets (livros[a].categoria);
+        printf ("Informe o preço: ");
+        scanf ("%f", &livros[a].preco);
+        
+        printf ("\n");
+        fflush (stdin);
+    }
 
 // Tela de resultado
 
 system ("cls || clear");
 
-    printf ("Nome: %s\n", Dados.nome);
-    printf ("Idade: %d\n", Dados.idade);
-    printf ("Endereço: %s\n", Dados.endereco);
+    for (a = 0; a < 3; a++) {
+        printf ("Nome do %dº livro: %s\n", a+1, livros[a].nome);
+        printf ("Autor do livro: %s\n", livros[a].autor);
+        printf ("Categoria do livro: %s\n", livros[a].categoria);
+        printf ("Preço do exemplar: R$ %.2f\n", livros[a].preco);
+        printf ("\n");
+    }
 
     return 0;
 }
