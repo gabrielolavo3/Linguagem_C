28b9bd5903d2a7de98518ec0029127c60c398536
Gabriel S. Olavo <143572039+gabrielolavo3@users.noreply.github.com>
2023-12-05 13:20:23 -0300

    Create 08. Acumulando valores.c

---
+++ Visual Studio Code - Linguagem C/02. Exercises at Home/#Exercícios de Vetores (Arrays)/Lista II de Aprendizagem/08. Acumulando valores.c
@@ -0,0 +1,42 @@
+/* 
+Descrição  :  Leia um vetor de 40 posições e acumular e imprimir os valores! (função)
+Autor(a)   : Gabriel S. Olavo
+Data atual : 05/12/2023 */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+// Declaração de constante
+
+#define G 40
+
+int main ( ) {
+  setlocale (LC_ALL, "portuguese");
+
+// Declaração de variáveis
+
+  int a;
+  float vetor[G], soma;
+
+// Solicitando dados
+
+  for (a = 0; a < G; a++) {
+    printf ("Digite o %dº valor: ", a+1);
+    scanf ("%f", &vetor[a]);
+
+    soma += vetor[a];
+  }
+
+// Tela de resultado
+
+system ("cls || clear");
+  
+  for (a = 0; a < G; a++) {
+    printf ("%dº valor: %.2f\n", a+1, vetor[a]);
+  }
+
+  printf ("Soma dos valores: %.2f\n", soma);
+  
+  return 0;
+}
