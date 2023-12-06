a07a980da9dc8e2fb5efba30af4b64c724db4966
Gabriel S. Olavo <143572039+gabrielolavo3@users.noreply.github.com>
2023-12-06 18:32:02 -0300

    Update and rename 02. Preenchimento de vetor com ímpares.c to 03. Preenchimento de vetor com ímpares.c

--- Visual Studio Code - Linguagem C/02. Exercises at Home/#Exercícios de Vetores (Arrays)/03. Preenchimento de vetor com ímpares.c
+++ Visual Studio Code - Linguagem C/02. Exercises at Home/#Exercícios de Vetores (Arrays)/03. Preenchimento de vetor com ímpares.c
@@ -1,5 +1,5 @@
 /* 
-Descrição  : Declarar um vetor de 10 posições e o preenchee imprimrir com os 10 primeiros números impares! (função)
+DescriÃ§Ã£o  : Declarar um vetor de 10 posiÃ§Ãµes e o preenchee imprimrir com os 10 primeiros nÃºmeros impares! (funÃ§Ã£o)
 Autor(a)   : Gabriel S. Olavo
 Data atual : 27/11/2023 */
 
@@ -7,14 +7,14 @@ Data atual : 27/11/2023 */
 #include <stdlib.h>
 #include <locale.h>
 
-// Declaração de constante
+// DeclaraÃ§Ã£o de constante
 
 #define b 10
 
 int main ( ) {
     setlocale (LC_ALL, "portuguese");
 
-// Declaração de variáveis
+// DeclaraÃ§Ã£o de variÃ¡veis
 
     int a;
     int vetor[] = {1,3,5,7,9,11,13,15,17,19};
